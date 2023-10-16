78c778ebd31ed7bd2ee16b6ddd698dabcf7f1d5c
Jorge Duarte  Marin <jorgeivanduartemarin6b@gmail.com>
2023-10-16 18:09:51 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
-const app = express()
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
    res.send("Hello word")
 })
 
 app.listen(3000)
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
